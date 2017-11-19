ca62f8ae16f5622ca5f21811b1dcbbcefa6df942
Ivelina Hristova <iv4etoxx@gmail.com>
2017-11-19 22:22:16 +0200

    Js packages, node_modules, gulpfile

---
+++ public/gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var minifycss = require('gulp-minify-css');
+
+gulp.task('css', function() {
+    return gulp.src([
+        'node_modules/bootstrap/dist/css/bootstrap.css',
+        'node_modules/font-awesome/css/font-awesome.min.css',
+        'vendor/sb/blog-post.css'
+        ])
+        .pipe(minifycss())
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('default', function() {
+    gulp.run('css', 'js_admin');
+});
